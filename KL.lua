e21130aa3fc921b290a98ec548a442027f4732ff
PowerxCANDYYY <94974926+PowerxCANDYYY@users.noreply.github.com>
2022-06-25 19:43:51 +0700

    Update KL.lua

--- KL.lua
+++ KL.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("\n ไป /getscript ในห้อง BotCommand ในดิส Normal Hub \n /getscript in Chat BotCommannd Discord Normal Hub")
+game.Players.LocalPlayer:Kick("\n ไป /getscript ในห้อง BotCommand ในดิส Normal Hub")
